03ab45aba7c930234a24a3829b357bfdd1777df1
Kanishk Srivastava <110916934+KanishkSrivastava15@users.noreply.github.com>
2024-04-07 02:10:34 -0400

    Add files via upload
    WallStreet Man files(index.html , style.css , word-list.js , script.js , and images/gifs used

---
+++ word-list.js
@@ -0,0 +1,42 @@
+const wordList = [
+    {
+        word: "blockchain",
+        hint: "A decentralized, distributed ledger technology used in cryptocurrencies."
+    },
+    {
+        word: "liquidity",
+        hint: "The ease with which an asset can be converted into cash without affecting its market price."
+    },
+    {
+        word: "budget",
+        hint: "A plan for managing income and expenses over a specific period of time."
+    },
+    {
+        word: "dividend",
+        hint: "A payment made by a corporation to its shareholders, usually as a distribution of profits."
+    },
+    {
+        word: "inflation",
+        hint: "The rate at which the general level of prices for goods and services is rising."
+    },
+    {
+        word: "portfolio",
+        hint: "A collection of investments owned by an individual or organization."
+    },
+    {
+        word: "mining",
+        hint: "Process of validating transactions and adding them to a blockchain."
+    },
+    {
+        word: "immutable",
+        hint: "A characteristic of blockchain data that refers to its inability to be changed or tampered with once recorded."
+    },
+    {
+        word: "token",
+        hint: "A digital asset representing ownership or access rights, often used in blockchain-based projects."
+    },
+    {
+        word: "exchange",
+        hint: "A platform where assets like cryptocurrencies or securities can be bought and sold."
+    },
+]
